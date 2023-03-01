9ca89ff097d06431030b080f33283ba5f8942b82
MatisseBE <51272243+MatisseBE@users.noreply.github.com>
2023-03-01 22:02:41 +0100

    Add files via upload

---
+++ Create AIRSPACE from ESE.py
@@ -0,0 +1,230 @@
+import json
+import re
+
+
+# A file containing only SECTOR data, limited noise (white lines, comments) is allowed. 
+with open('File With Sectors for AIRSPACE.txt',"r",encoding='utf-8') as f:
+    # File like:
+    # SECTOR:EBBU·EBBE TMA1A·025·035:02500:03500
+    # OWNER:MIL:BI
+    # BORDER:114:115:116:117:118:119:120:121:122:123:124:125
+
+    sectors = f.readlines() 
+
+# Converts more than two white spaces into exactly two white spaces (necessary for splitting)
+def filter_string(string):
+    return re.sub(r'(\n){3,}', r'\n\n', string)
+
+# Removes comments from file
+def cleanup(file):
+    textfile = ""
+
+    for line in file:
+        line = line.split(';')[0]
+
+        if line != "":
+            textfile += line.strip() + "\n"
+    
+    return filter_string(textfile)
+
+sectors = cleanup(sectors)
+sectors = sectors.split("\n\n")
+
+
+sectors = [i for i in sectors if i] # only keeping the items (sectors) that are truthy, that is, not empty strings or None values
+
+sectordic = {}
+
+# Removes string OWNER from the line and returns all sector owners' names/ids
+def splitowners(line):
+    line = [x for x in line if x.startswith("OWNER:")]
+    try:
+        return line[0].split(":")[1:]
+    except:
+        return line[0]
+
+# Removes string BORDER from the line and returns all borderlines' names/numbers
+def splitborders(line):
+    line = [x for x in line if x.startswith("BORDER:")]
+    try:
+        return line[0].split(":")[1:]
+    except:
+        return line[0]
+    
+# Dictionary with information about the sectors (no coordinates yet, only border names/numbers)
+for sector in sectors:
+    line = sector.split("\n")
+
+    name = line[0].split(":")[1]
+    low = line[0].split(":")[2]
+    high = line[0].split(":")[3]
+    owners = splitowners(line)
+    borders = splitborders(line)
+    
+    sectordic[name] = {
+        "low": low,
+        "high" : high,
+        "owners" : owners,
+        "borders": borders,
+    }
+
+# A file containing only SECTORLINES, limited noise (white lines, comments) is allowed. 
+with open('File With Sectorlines for AIRSPACE.txt') as f:
+    # File like:
+    # SECTORLINE:572
+    # COORD:502517:0004917
+    # COORD:502405:0004611
+
+    textfile = f.readlines()
+
+sectorlines = cleanup(textfile)
+sectorlines = sectorlines.split("\n\n")
+
+# Formats all coordinates of a given sectorline
+def getcoor(line):
+    coorlines = [x for x in line if x.startswith("COORD:")]
+    coors = []
+    for coorline in coorlines:
+        coorline = coorline.replace("COORD:", "")
+        coorline = [coorline.split(":")[0],coorline.split(":")[1]]
+        coors.append(coorline)
+    return coors
+        
+
+# Dictionary of linedic<sectorline_name> = {coor : [[bla,blabla][bla,bla,bla]]}
+linedic = {}
+for sectorline in sectorlines:
+    if sectorline.startswith("\n"):
+        sectorline = sectorline[2:]
+
+    lines = sectorline.split("\n")
+    coor = getcoor(lines)
+    
+    name = lines[0].split(":")[1]
+    
+    linedic[name] = {
+        "coor" : coor
+    }
+        
+# Connects all sector lines into one big line
+def chain(dominoes):
+    #print("\n",dominoes)
+    #print("Before", linedic["176"])
+    for i in range(len(dominoes) - 1):
+        for j in range(len(dominoes) - 1):
+            j += i + 1
+
+            if dominoes[i][-1] == dominoes[j][0]: # head == tail
+                #print("head=tail",dominoes[i],dominoes[j])
+                rev = dominoes[j]
+                new_list = dominoes[i] + rev
+                dominoes[i] = new_list
+                #dominoes[i].extend(rev)
+                dominoes.remove(dominoes[j])
+            
+                if len(dominoes) == 1:
+                    return dominoes[0]
+                else:
+                    return chain(dominoes)
+
+            elif dominoes[i][-1] == dominoes[j][-1]: # head == head
+                #print("head=head",dominoes[i][-1],dominoes[i],dominoes[j])
+                rev = dominoes[j][::-1]
+                new_list = dominoes[i] + rev
+                dominoes[i] = new_list
+                #dominoes[i].extend(rev)
+                dominoes.remove(dominoes[j])
+                
+                if len(dominoes) == 1:
+                    return dominoes[0]
+                else:
+                    return chain(dominoes)
+
+            elif dominoes[i][0] == dominoes[j][0]: # tail == tail
+                #print("tail=tail",dominoes[i][0],dominoes[i],dominoes[j])
+                dominoes[i] = dominoes[j][::-1] + dominoes[i]
+                dominoes.remove(dominoes[j])
+                
+                if len(dominoes) == 1:
+                    return dominoes[0]
+                else:
+                    return chain(dominoes)
+
+            elif dominoes[i][0] == dominoes[j][-1]: # tail == head
+                #print("tail=head",dominoes[i][0],dominoes[i],dominoes[j])
+                dominoes[i] = dominoes[j] + dominoes[i]
+                dominoes.remove(dominoes[j])
+                
+                if len(dominoes) == 1:
+                    return dominoes[0]
+                else:
+                    return chain(dominoes)
+
+#Creates a nested list of the secotors' coordinates
+def getpoints(borders):
+    coordinates = []
+    for b in borders:
+        coordinates.append(linedic[b]["coor"])
+
+    coordinates_copy = coordinates.copy()
+
+    return chain(coordinates_copy)
+
+
+WordOfGodFinalDict = {}
+airspaces = []
+
+# Used to assign the sector a group based on the sectors' name
+def findname(sector):
+    sector = sector.split("·")[1].replace("_SCT", "")
+
+    if sector.startswith("EB"):
+        return "EBBU"
+
+    elif sector.startswith("EL"):
+        return "ELLX"
+    
+    elif sector.startswith("EDYY"):
+        return "EDYY"
+    
+    elif sector.startswith("CBWV"):
+        return "CBWV"
+
+    elif sector.startswith("ZEELAND"):
+        return "ZEELAND"  
+
+    elif sector.startswith("ACI"):  
+        return "ACI"
+    
+    ## Do NOT REMOVE, this is your fail safe - Do **not** add this key to your groups json
+    else:
+        return "OTHER"
+
+# Creates the output and is the heart of the code
+for sector in sectordic.keys():
+    try:
+        print(sector)
+        name = sector.split("·")[1].replace("_SCT", "")
+        
+        tmp = {}
+        tmp["id"] = name
+        tmp["group"] = findname(sector)
+        tmp["owner"] = sectordic[sector]["owners"]
+        tmp["sectors"] = [ {
+            "min" : int(int(sectordic[sector]["low"])/100) ,
+            "max": int(int(sectordic[sector]["high"])/100) -1,
+            "points" : getpoints(sectordic[sector]["borders"])
+        }]
+
+        if tmp["sectors"][0]["points"] != None:
+                    airspaces.append(tmp)
+
+    except:
+        print("FAILED", sector)
+        continue
+
+WordOfGodFinalDict["airspace"] = airspaces
+
+# Directly from dictionary
+with open('./Files/airspace.json', 'w') as outfile:
+    json.dump(WordOfGodFinalDict, outfile)
---
+++ readme.md
@@ -0,0 +1,103 @@
+
+## File preparations
+
+  
+
+#### 'Your GNG ESE file'
+
+1. In your text editor, perform a find and replace using REGEX (in VSC enable this icon:)![show regex symbol](https://cdn.discordapp.com/attachments/1071509192680153239/1071831914526298203/image.png)
+This action will convert all your ES coordinates to the coordinate system used by vatglasses.
+**Attention**
+A) Convert for all hemispheres NESW (North, East, South, West). Currently, only coordinates that are in the NE hemisphere are converted. Perform as many find and replaces as neccessary.
+B) Check if the delimiter between two hemispheres is correct in the REGEX. Here ':' is used (character before "E"). Adapt if neccesarry and/or perform as many find and replaces as neccessary.
+```
+Find: N0?([0-9]{2}).([0-9]{2}).([0-9]{1,2})(.[0-9]{3,})?:E([0-9]{3}).([0-9]{2}).([0-9]{1,2})(.[0-9]{3,})?
+Replace: $1$2$3:$5$6$7
+```
+
+  Example:
+```
+ESE coordinate line
+COORD:N51.01.22:E005.13.15
+COORD:N51.02.16:E005.05.08
+New coordinate line with vatglasses coordinate system
+COORD:510122:0051315
+COORD:510216:0050508
+```
+ 
+### 'File with sectors for airspace.txt'
+This file will contain the sector data from ESE. Take a look at the example.
+This are all the sectors with their corresponding owners and sectorlines (borders).
+From the GNG ESE, copy all **sector data** to this file as shown in the example. It may include comments. 
+
+### 'File with sectorslines for airspace.txt'
+This file will contain the **border coordinates** from ESE. Take a look at the example. 
+These are all the borders that surround a sector (at a certain FL-block).
+From the GNG ESE, copy all **sectorlines** to this file as shown in the example. It may include comments. 
+
+### 'File with positions for positions.txt'
+This file will contain **positions** from the ESE. Take a look at the example. 
+From the GNG ESE, copy all **positions** to this file as shown in the example. It may include comments. 
+
+ 
+### Open 'create airspace from ESE .py' file
+This file creates the "airspace" key in the vatglass json.
+Here will will need to make some adjustments so the file can run without any problems. 
+
+On line 178 you will find the function *Findname(sector)*. This function is used to assign a sector to a group based on the name of the sector. The function strips the sector name and checks to which group it must belong and assigns it. You will need to change this function to your needs and groups. Don't forget to also change the first line.
+
+**Attention**
+"groups" Is another key in the vatglasse's json. You will need to create this part of the json manually. Make sure the groupkeys are identical between the function and json. Corresponding example for Belux in the vatglasse's json: 
+```
+"groups":{
+	"EBBU":{
+	"name":"Brussels"
+	},
+	"ELLX":{
+	"name":"Luxembourg"
+	},
+	"EDYY":{
+	"name":"Maastricht"
+	},
+...
+}
+```
+
+Under this the *Findname(sector)* function, line 203 for me, you will find a for loop. Change the *name* variable such that it works with your setup. Just like before the example takes the sectorname from ESE and makes some amendments.
+
+Awesome! You can now run the file!
+
+### Open 'create POSITIONS from ESE .py' file
+This file creates the "positions" key in the vatglass json.
+The *similarhex(position)* function is an attempt at giving every position group (TWR/APP/ACC) a color group with every position of that group having a different shade.  This function is pretty bad and that's okay. You can change the colors of sectors afterwards or play with the function yourself. You might want to change the initial default color groups though, but if not, it will all still work just fine.
+
+If you want to (include or) exclude a certain position like, amend line 88. 
+
+### Open 'create a PART OF AIRPORTS .py' file
+This file does most of the work for the "airports" key. Use [vatspy-data-project](https://github.com/vatsimnetwork/vatspy-data-project/blob/master/VATSpy.dat) to get name, callsign and coordinates of all the controlled airports. Paste it in the Python file and run it. 
+**Important** 
+Add a topdown structure!  (It's actually **bottum up** in this case - starting with the lowest radar position and **not TWR**. I.e.: BA is the controller ID for Brussels Arrival)
+ ```
+ "EBBR":{
+"callsign":"Brussels",
+"coord":[50.9013,4.4901],
+"topdown":["BA","BD","BW","BE"]
+}
+```
+Optional: If there is no topdown structure, or the airport in uncontrolled, you can add run the file again and add the value of the output straight to the vatglass json without a new key.
+![show regex symbol](https://cdn.discordapp.com/attachments/727230055301906483/1080591518273900565/image.png)
+
+### What's next?
+1. Create a json entry with keys:
+* airspace - You made this with the script.
+* groups - As explained earlier (check example). 
+	* Make sure that NO sector has been assigned group "OTHER". You will need to check your *findname(sector)* function and make sure that all sectors have a condition made for them.
+	* OTHER may not be a key in your groups json. 
+* positions - You made this with the script.
+* callsigns - You can probably copy paste a large portion of this one from other vACCs (Check example).
+* airports - You partly made this with a script
+* (the optional uncontrolled airports as explained under Open 'create a PART OF AIRPORTS .py' file.)
+
+2. Next, format the file using the Prettier - Code formatter extention in VSC.
+3. Upload under the assigned file in the beta repo as assigned by vatglasse's dev. 
+
